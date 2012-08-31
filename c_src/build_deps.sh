89119e4e5bb4cf67f99ebcceaea8297a7937cadc
xinming.yao <yaoxinming@gmail.com>
2012-08-31 10:06:16 +0800

    modify build script

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -8,7 +8,7 @@ if [ `uname -s` = 'SunOS' -a "${POSIX_SHELL}" != "true" ]; then
 fi
 unset POSIX_SHELL # clear it so if we invoke other scripts, they run as ksh as well
 
-LEVELDB_VSN="gc_manager_dev" # June 25 merge w/ mv-write-throttle2
+LEVELDB_VSN="master" # June 25 merge w/ mv-write-throttle2
 SNAPPY_VSN="1.0.4"
 
 set -e
